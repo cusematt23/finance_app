64a6501b10e60b4539d19f8759d35beccaba22e7
Matt Moore <Moore.MatthewPaul@gmail.com>
2023-10-19 12:04:28 -0400

    fixed a few errors in app_03

--- 03_placeholder_app/app.py
+++ 03_placeholder_app/app.py
@@ -1,19 +1,30 @@
-#IMPORTS
-from shiny import App, render, ui, reactive, Session, req
-from shinywidgets import output_widget, render_widget
-import jinja2
+#LEARNING LAB
+#PART3 SHINY FOR PYTHON
+#https://www.youtube.com/watch?v=taZYIqC7VMM&ab_channel=BusinessScience
 
+#Goal: Create static data analysis
+
+#IMPORTS
+from shiny import (
+    App, render, ui, reactive, Session,
+    req
+)
+from shinywidgets import (
+    output_widget, render_widget
+)
 import numpy as np
 import pandas as pd
 import yfinance as yf
 import plotly.express as px
 import plotly.graph_objects as go
 from pathlib import Path
-
+import jinja2
 #DEFAULT APP INPUTS
-TITLE="PyShiny Financial Analysis"
 
-#DATA INGEST
+TITLE="PyShiny Financial Equity Analysis"
+
+#DEFAULT DATA FOR WHEN PROGRAM BEGINS
+
 symbol='MSFT'
 period="5y"
 window_mavg_short=30
@@ -29,7 +40,7 @@ stock_history = stock.history(period=period)
 #https://getbootstrap.com/docs/5.0/components/card/
 def my_card(title, value, width=4, bg_color="bg-info", text_color="text-white"):
     """
-    Quick function to create a bootstrap card
+    Quick function to create a bootstrap 5 card
     """
     card_ui = ui.div(
         ui.div(
@@ -38,19 +49,31 @@ def my_card(title, value, width=4, bg_color="bg-info", text_color="text-white"):
                 ui.div(value, class_="card-text"),
                 class_="card-body flex-fill"
             ),
-            class_ = f"card {text_color} {bg_color}", style="flex:grow:1;margin:5px;"
-        )
+            class_ = f"card {text_color} {bg_color}",
+            style="flex-grow:1;margin:5px;"
+        ),
+        class_=f"col-md-{width} d-flex"
     )
-    return card_ui
 
+    return card_ui
 
 def make_plotly_chart(stock_history, window_mavg_short=30, window_mavg_long=90):
     """
     Function to create a plotly chart
     """
+
+    #Hello, just matching his rows line for line
+
     stock_df=stock_history[['Close']].reset_index()
+
+
     stock_df['mavg_short']=stock_df['Close'].rolling(window=window_mavg_short).mean()
+
+
+
     stock_df['mavg_long']=stock_df['Close'].rolling(window=window_mavg_long).mean()
+
+
     
     fig = px.line(
         data_frame=stock_df.set_index('Date'),
@@ -59,9 +82,9 @@ def make_plotly_chart(stock_history, window_mavg_short=30, window_mavg_long=90):
             "mavg_short": "#0d6efd",
             "mavg_long": "#0dcaf0"
         },
-        #title=f"{symbol} Stock Chart"
         title=None
     )
+
     fig = fig.update_layout(
         plot_bgcolor='rgba(0,0,0,0)',
         paper_bgcolor='rgba(0,0,0,0)',
@@ -75,15 +98,15 @@ def make_plotly_chart(stock_history, window_mavg_short=30, window_mavg_long=90):
     )
 
     fig = fig.update_xaxes(
-        title='',
+        title="",
         gridcolor="#2c3e50"
     )
 
     return fig
 
 
-
 #LAYOUT
+
 page_dependencies = ui.tags.head(
     ui.tags.link(rel="stylesheet", type="text/css",href="style.css")
 )#rel=relationship to external file, type=type of file, href=location of file
@@ -95,7 +118,13 @@ app_ui = ui.page_navbar(
         ui.layout_sidebar(
             sidebar=ui.panel_sidebar(
                 ui.h2("Select a Stock"),
-                ui.input_selectize("stock_symbol", "Stock Symbol", ['AAPL','GOOG', 'MSFT'], selected='MSFT', multiple=False),
+                ui.input_selectize(
+                    "stock_symbol",
+                    "Stock Symbol",
+                    ['AAPL','GOOG', 'MSFT'],
+                    selected='MSFT',
+                    multiple=False
+                    ),
                 width=3
             ),
             main=ui.panel_main(
@@ -103,7 +132,7 @@ app_ui = ui.page_navbar(
                     ui.output_text("txt")
                 ),
                 ui.div(
-                    output_widget("stock_summary", width="auto", height="auto"),
+                    output_widget("stock_chart_widget", width="auto", height="auto"),
                    class_="card"
                 ),
                 ui.navset_pill_card(
@@ -115,9 +144,9 @@ app_ui = ui.page_navbar(
                         "Income Statement",
                         ui.output_table("income_statement_table")   
                     )
-                )
+                ),
             )
-        )
+        ),
     ),
     title=ui.tags.div(
         ui.img(src="shipit.png", height="30px", style="margin:5px;"),
@@ -130,8 +159,8 @@ app_ui = ui.page_navbar(
 )
 
 
-
 def server(input, output, session:Session):
+
     @output
     @render.text
     def txt():
@@ -141,19 +170,20 @@ def server(input, output, session:Session):
     @output
     @render.ui
     def stock_info_ui():
+        #stock_info = stock().info
         app_ui = ui.row(
+
             #Company info
             ui.h5("Company Information"),
             my_card("Industry", stock_info['industry'], bg_color="bg-dark"),
             my_card("Fulltime Employees", "{:0,.0f}".format(stock_info['fullTimeEmployees']), bg_color="bg-dark"),
-            my_card("Website", ui.a(stock_info['website'],href=stock_info['website'],
-            target="_blank"), bg_color="bg-dark"),
+            my_card("Website", ui.a(stock_info['website'],href=stock_info['website'],target="_blank"),bg_color="bg-dark"),
 
             ui.hr(),
 
             #Financial Ratios
             ui.h5("Financial Ratios"),
-            my_card("Profit Margins", "{:0,.1%}".format(stock_info['profitMargins']), bg_color="bg-primary"),
+            my_card("Profit Margin", "{:0,.1%}".format(stock_info['profitMargins']), bg_color="bg-primary"),
             my_card("Revenue Growth", "{:0,.1%}".format(stock_info['revenueGrowth']), bg_color="bg-primary"),
             my_card("Current Ratio", "{:0,.2f}".format(stock_info['currentRatio']), bg_color="bg-primary"),
 
@@ -163,7 +193,8 @@ def server(input, output, session:Session):
             ui.h5("Financial Operations"),
             my_card("Total Revenue", "${:0,.0f}".format(stock_info['totalRevenue']), bg_color="bg-info"),
             my_card("EBITDA", "${:0,.0f}".format(stock_info['ebitda']), bg_color="bg-info"),
-            my_card("Operating Cash Flow", "${:0,.0f}".format(stock_info['operatingCashflow']), bg_color="bg-info")
+            my_card("Operating Cash Flow", "${:0,.0f}".format(stock_info['operatingCashflow']), bg_color="bg-info"),
+
         )
         return app_ui
     
@@ -171,14 +202,17 @@ def server(input, output, session:Session):
     @output
     @render_widget
     def stock_chart_widget():
-        fig = make_plotly_chart(stock_history, window_mavg_short=30, window_mavg_long=90)
+        fig = make_plotly_chart(stock_history, window_mavg_short, window_mavg_long)
         return go.FigureWidget(fig)
     
     #Build income statement table
     @output
     @render.table
     def income_statement_table():
-        return stock_incomestmt.reset_index()
+        return (
+            stock_incomestmt \
+                .reset_index()
+        )
     #end server()
 
 
@@ -187,3 +221,11 @@ www_dir = Path(__file__).parent / "www"
 
 
 app = App(app_ui, server, static_assets=www_dir)
+
+
+
+
+#REACTIVITY
+#@reactive.Calc
+#def stock():
+#    return yf.Ticker(str(input.stock_symbol()))
